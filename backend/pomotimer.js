237e2e32471957ddcd6d2c238b66d22837c2fd32
ryandomalley <rd.omalley@comcast.net>
2020-05-22 14:55:16 -0500

    OOP Implementation
    - Moved everything into a Clock class
    - Implementated a resume method
    - Increased comments
    - Fixed millisecond conversion
    - Changed status flags to type string to assist clarity.

--- backend/pomotimer.js
+++ backend/pomotimer.js
@@ -5,125 +5,140 @@ const resetButton = document.getElementById("reset");
 const minuteDisplay = document.getElementById("minutes");
 const secondDisplay = document.getElementById("seconds");
 
-var timerMinutes = 0;
-var timerSeconds = 5;
+let timerMinutes = 0;
+let timerSeconds = 5;
 
-var remainingMinutes = 0;
-var remainingSeconds = 0;
-
-/*
-Status codes:
-0 - not started
--1 - completed
-1 - running
-2 - paused
-*/
-var status = 0;
-
-var clockHandle;
-
-function countDown(minutes,seconds){
-    status = 1;
-    var end = new Date();
-    end.setMinutes(end.getMinutes() + minutes);
-    end.setSeconds(end.getSeconds() + seconds);
-    function tick(){
-        //Get current date
-        var now = new Date();
-        //Calculate remaining milliseconds
-        var remainingMilli = end - now;
-        //Check to see if the timer has elapsed, or the reset button has been pressed
-        //Stop the clock if either happen
-        if(remainingMilli <= 0){
-            clearInterval(clockHandle);
-            remainingMinutes = 0;
-            remainingSeconds = 0;            
-            updateClock();
-            status = -1;
-            //Exit the function
-            return;
-        }
-        //Convert milliseconds to Minutes + Seconds, then update variables
-        timeConversion(remainingMilli);
-        //Update hmtl
-        updateClock();
+class Clock{
+    constructor(){
+        this.remainingMinutes = 0;
+        this.remainingSeconds = 0;
+        this.status = "not started";
     }
-    clockHandle = setInterval(tick, 90);
-}
 
-function timeConversion(milliseconds){
-    remainingMinutes = Math.floor(milliseconds / 60000);
-    remainingSeconds = ((milliseconds % 60000) / 1000).toFixed(0);
-}
+    countDown(minutes,seconds){
+        this.status = "running";
+        let end = new Date();
+        end.setMinutes(end.getMinutes() + minutes);
+        end.setSeconds(end.getSeconds() + seconds);
+        // Had to use an arrow function, otherwise a function inside the countDown() method won't be able to access
+        // the other class methods. We love javascript!
+        this.clockHandle = setInterval(() =>{
+            //Get current date
+            let now = new Date();
+            //Calculate remaining milliseconds
+            let remainingMilli = end - now;
+            //Check to see if the timer has elapsed, or the reset button has been pressed
+            //Stop the clock if either happen
+            if(remainingMilli <= 0){
+                clearInterval(this.clockHandle);
+                this.remainingMinutes = 0;
+                this.remainingSeconds = 0;            
+                this.updateClock();
+                this.status = "completed";
+                //Exit the function
+                return;
+            }
+            else{
+                //Convert milliseconds to Minutes + Seconds, then update HTML
+                this.timeConversion(remainingMilli);
+                //Update hmtl
+                this.updateClock();
+            }
+        }, 45);
+    }
 
-/* 
-updateClock() : Updates the display to latest remaining time
-@param : none
-@return : none
-*/
-function updateClock(){
-    minuteDisplay.innerHTML = remainingMinutes;
-    secondDisplay.innerHTML = remainingSeconds;
-}
-
-/* 
-resetClock() : Resets the countdown timer. If running first stops the clock, before resetting.
-@param : none
-@return : none
-*/
-function resetClock(){
-    /*
-    A running clock must first be stopped before it can be reset.
-    Status code 1 tells us that the clock is running.
+    /* 
+    timeConversion() : Converts milliseconds to remainingMinutes and remainingSeconds
+    @param : number of milliseconds to convert
+    @return : none
     */
-    if(status == 1){
-        clearInterval(clockHandle); //Disable clock
-        remainingMinutes = 0;
-        remainingSeconds = 0;
+    timeConversion(milliseconds){
+        this.remainingMinutes = ~~(milliseconds / 60000);
+        let remainingMilliseconds = milliseconds - (this.remainingMinutes*60000)
+        this.remainingSeconds = ~~(remainingMilliseconds / 1000);
     }
-    minuteDisplay.innerHTML = timerMinutes;
-    secondDisplay.innerHTML = timerSeconds;
 
-    pauseButton.style.display = "none";
-    resumeButton.style.display = "none";
-    startButton.style.display = "inline";
-
-    status = 0;
-}
-/* 
-startClock() : Starts the timer if and only if the clock has not been started (status code 0).
-@param : none
-@return : none
-*/
-function startClock(){
-    if(status == 0){
-        countDown(timerMinutes,timerSeconds);
-        startButton.style.display = "none";
-        pauseButton.style.display = "inline";
+    /* 
+    updateClock() : Updates the display to latest remaining time
+    @param : none
+    @return : none
+    */
+    updateClock(){
+        minuteDisplay.innerHTML = this.remainingMinutes;
+        secondDisplay.innerHTML = this.remainingSeconds;
     }
-}
 
-function pauseClock(){
-    if(status == 1){
-        clearInterval(clockHandle);
+    /* 
+    resetClock() : Resets the countdown timer. If running first stops the clock, before resetting.
+    @param : none
+    @return : none
+    */
+    resetClock(){
+        /*
+        A running clock must first be stopped before it can be reset.
+        Status code 1 tells us that the clock is running.
+        */
+        if(this.status === "running"){
+            clearInterval(this.clockHandle); //Disable clock
+            this.remainingMinutes = 0;
+            this.remainingSeconds = 0;
+        }
+        minuteDisplay.innerHTML = timerMinutes;
+        secondDisplay.innerHTML = timerSeconds;
+
         pauseButton.style.display = "none";
-        resumeButton.style.display = "inline";
-        status = 2;
-    }
-}
-
-function resumeClock(){
-    if(status == 2){
-        console.log(remainingMinutes);
-        console.log(remainingSeconds);
-        countDown(remainingMinutes,remainingSeconds);
-        pauseButton.style.display = "inline";
         resumeButton.style.display = "none";
+        startButton.style.display = "inline";
+
+        this.status = "not started";
+    }
+    /* 
+    startClock() : Starts the timer if and only if the clock has not been started.
+    @param : none
+    @return : none
+    */
+    startClock(){
+        if(this.status === "not started"){
+            this.countDown(timerMinutes,timerSeconds);
+            startButton.style.display = "none";
+            pauseButton.style.display = "inline";
+        }
+    }
+    /* 
+    pauseClock() : Pause the timer only if the clock is running.
+    @param : none
+    @return : none
+    */
+    pauseClock(){
+        if(this.status === "running"){
+            clearInterval(this.clockHandle);
+            pauseButton.style.display = "none";
+            resumeButton.style.display = "inline";
+            this.status = "paused";
+        }
+    }
+
+    /* 
+    resumeClock() : Resumes the timer if and only if the clock is paused.
+    @param : none
+    @return : none
+    */
+    resumeClock(){
+        if(this.status === "paused"){
+            this.countDown(this.remainingMinutes,this.remainingSeconds);
+            pauseButton.style.display = "inline";
+            resumeButton.style.display = "none";
+            this.status = "running";
+        }
     }
 }
 
-resetClock();
-startButton.addEventListener("click",startClock);
-resetButton.addEventListener("click",resetClock);
-pauseButton.addEventListener("click",pauseClock);
-resumeButton.addEventListener("click",resumeClock);
+
+let clock = new Clock();
+clock.resetClock();
+
+//resetClock();
+startButton.addEventListener("click",() => {clock.startClock() });
+resetButton.addEventListener("click",() => {clock.resetClock() });
+pauseButton.addEventListener("click",() => {clock.pauseClock() });
+resumeButton.addEventListener("click",() => {clock.resumeClock() });
